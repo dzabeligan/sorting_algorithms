301075e03b11f7ce0dffe952b98326cedc1daf33
elijah.balogun <elijah.balogun@iisysgroup.com>
2023-08-06 20:51:32 +0100

    feat: Implement insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,19 +6,18 @@
  * @n1: A pointer to the first node to swap.
  * @n2: The second node to swap.
  */
-void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
+void swap_nodes(listint_t **h, listint_t *n1, listint_t *n2)
 {
-	(*n1)->next = n2->next;
+	n1->next = n2->next;
 	if (n2->next != NULL)
-		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
-	if ((*n1)->prev != NULL)
-		(*n1)->prev->next = n2;
+		n2->next->prev = n1;
+	n2->prev = n1->prev;
+	n2->next = n1;
+	if (n1->prev != NULL)
+		n1->prev->next = n2;
 	else
 		*h = n2;
-	(*n1)->prev = n2;
-	*n1 = n2->prev;
+	n1->prev = n2;
 }
 
 /**
@@ -30,19 +29,39 @@ void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *iter, *insert, *tmp;
+	listint_t *iter, *next;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 
-	for (iter = (*list)->next; iter != NULL; iter = tmp)
+	for (iter = (*list)->next; iter != NULL; iter = next)
 	{
-		tmp = iter->next;
-		insert = iter->prev;
+		listint_t *insert = iter->prev;
+
+		next = iter->next;
 		while (insert != NULL && iter->n < insert->n)
 		{
-			swap_nodes(list, &insert, iter);
+			swap_nodes(list, insert, iter);
+			insert = iter->prev;
 			print_list((const listint_t *)*list);
+		}
+	}
+}
+
+void insertion_sort(int *arr, size_t length)
+{
+	size_t i, j;
+	int key;
+
+	for (i = 1; i < length; i++)
+	{
+		j = i;
+		while (j > 0 && arr[j - 1] > arr[j])
+		{
+			key = arr[j];
+			arr[j] = arr[j - 1];
+			arr[j - 1] = key;
+			j--;
 		}
 	}
 }
